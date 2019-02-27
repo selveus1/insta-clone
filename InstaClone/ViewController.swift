1d42c84912b5038a67dd73b3c0bd27b35fea1de4
selveus1 <fs031090@gmail.com>
2019-02-27 18:45:21 -0500

    Initial Commit

---
+++ InstaClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaClone
+//
+//  Created by Athena on 2/27/19.
+//  Copyright © 2019 Sheena Elveus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
