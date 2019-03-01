fd1c204071b5a49994e07f6261afab1c9f56249c
selveus1 <fs031090@gmail.com>
2019-03-01 14:58:52 -0500

    got the basic layout and ui down for login screen; working on fixing autolayout issues.

--- InstaClone/ViewControllers/LoginSignupViewController.swift
+++ InstaClone/ViewControllers/LoginSignupViewController.swift
@@ -13,6 +13,17 @@ class LoginSignupViewController: UIViewController {
 
     @IBOutlet var languageDropDown: DropDown!
     
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var loginSignupButton: UIButton!
+    @IBOutlet var getHelpLabel: UILabel!
+    
+    @IBOutlet var loginWithFBLabel: UILabel!
+    @IBOutlet var switchPerspectiveButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -30,9 +41,64 @@ class LoginSignupViewController: UIViewController {
         languageDropDown.textColor = UIColor.white
         languageDropDown.borderColor = UIColor.clear
         languageDropDown.selectedRowColor = UIColor.white
+        languageDropDown.rowHeight = 20
         
         
-        languageDropDown.rowHeight = 25
+        var dropDownTitle = NSMutableAttributedString()
+        let dropDownStr = "Language"
+        dropDownTitle = NSMutableAttributedString(string: dropDownStr, attributes: nil)
+        dropDownTitle.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.white, range:NSRange(location:0,length:dropDownStr.count))
+        languageDropDown.attributedPlaceholder = dropDownTitle
+        languageDropDown.layer.borderColor = UIColor.clear.cgColor
+        
+        
+        usernameTextField.backgroundColor = UIColor(red: 200.0/255.0, green: 200.0/255.0, blue: 200.0/255.0, alpha:0.25)
+        usernameTextField.textColor = .white
+    
+        //change placeholder text
+        var mutableStringTitle = NSMutableAttributedString()
+        let usernameString = "Username"
+        
+        mutableStringTitle = NSMutableAttributedString(string: usernameString, attributes: nil)
+        mutableStringTitle.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.white, range:NSRange(location:0,length:usernameString.count))
+        usernameTextField.attributedPlaceholder = mutableStringTitle
+        
+        var passwordStringTitle = NSMutableAttributedString()
+        let passwordString = "Password"
+        passwordStringTitle = NSMutableAttributedString(string: passwordString, attributes: nil)
+        passwordStringTitle.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.white, range:NSRange(location:0,length:passwordString.count))
+        passwordTextField.attributedPlaceholder = passwordStringTitle
+        
+        
+    
+        //pad text
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: usernameTextField.frame.height))
+        usernameTextField.leftView = paddingView
+        usernameTextField.leftViewMode = UITextField.ViewMode.always
+
+        passwordTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: passwordTextField.frame.height))
+        passwordTextField.leftViewMode = UITextField.ViewMode.always
+        
+        loginSignupButton.layer.borderWidth = 0.5
+        loginSignupButton.layer.borderColor = UIColor(red: 230.0/255.0, green: 230.0/255.0, blue: 230.0/255.0, alpha:0.25).cgColor
+        
+        
+        //imbed img into fb label
+        
+//        let imgPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: loginWithFBLabel.frame.height + 5, height: loginWithFBLabel.frame.height))
+//        loginWithFBLabel.addSubview(imgPaddingView)
+        
+        
+        let fbLogo: UIImage = UIImage(named: "FacebookLogoWhite")!
+        var bgImg: UIImageView?
+        bgImg = UIImageView(image: fbLogo)
+        bgImg!.frame = CGRect(x: 0, y: 0, width: loginWithFBLabel.frame.height, height: loginWithFBLabel.frame.height)
+        
+        loginWithFBLabel.addSubview(bgImg!)
+        loginWithFBLabel.textColor = .white
+        
+        switchPerspectiveButton.backgroundColor = UIColor(red: 200.0/255.0, green: 200.0/255.0, blue: 200.0/255.0, alpha:0.25)
+        
     }
     
 
